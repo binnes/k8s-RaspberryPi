e0ca883c992c93bce6dae235899378c92a95f189
brian111 <binnes@uk.ibm.com>
2019-01-23 13:45:15 +0000

    typo

--- scripts/regenPiCluster.py
+++ scripts/regenPiCluster.py
@@ -52,7 +52,7 @@ for sysType in config["testMachines"]["systems"]:
                 os.system('cp ' + fsRoot + '/' + sysType["bootImage"] + ' ' + imageName+'.gz')
                 os.system('gzip -d ' + imageName+'.gz')
                 os.system('kpartx -a ' + imageName)
-                p = subprocess.Popen('ls /dev/mapper | grep loop', stdout=subprocess.PIPE, shell=True)p = subprocess.Popen("date", stdout=subprocess.PIPE, shell=True)
+                p = subprocess.Popen('ls /dev/mapper | grep loop', stdout=subprocess.PIPE, shell=True)
                 (loopDev, err) = p.communicate()
                 p_status = p.wait()
                 os.system('mount')
