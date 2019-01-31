92105c4e7d6a8e7303bb182b551c8bb230124c6a
brian111 <binnes@uk.ibm.com>
2019-01-31 12:23:54 +0000

    enable local docker cache

--- scripts/createKubeCluster.py
+++ scripts/createKubeCluster.py
@@ -31,6 +31,13 @@ def runRemoteCommandWithReturn(host, cmd):
 
 def prepareKubeHost(config, host):
     runRemoteCommand(host, "curl -sSL get.docker.com | sh && sudo usermod pi -aG docker")
+    if "DockerCache" in config['testMachines']:
+        runRemoteCommand(host, """echo '''
+{
+    "registry-mirrors": ["http://{}}"],
+    "insecure-registries" : ["{}}"]
+}
+''' | sudo tee /etc/docker/daemon.json""".format(config['testMachines']["DockerCache"], config['testMachines']["DockerCache"]))
     runRemoteCommand(host, "sudo dphys-swapfile swapoff && sudo dphys-swapfile uninstall && sudo update-rc.d dphys-swapfile remove")
     runRemoteCommand(host, "echo -n ' cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory' | sudo tee -a /boot/cmdline.txt")
     runRemoteCommand(host, "sudo reboot -n")
